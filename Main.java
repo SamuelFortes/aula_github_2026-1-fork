1934d976931a37985e155aa3687a00ba289b7b9e
Diogo de Oliveira Ribeiro Brito <diogooliveiraribeiro76@gmail.com>
2026-05-31 17:34:53 -0300

    implementado salvamento em arquivo

--- Main.java
+++ Main.java
@@ -1,16 +1,24 @@
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Main {
 
 	private static final Map<Integer, Conta> contas = new HashMap<>();
+	private static final String CAMINHO_ARQUIVO = "contas.txt";
 
 	public static void main(String[] args) {
+		carregarDados();
 		Menu mainMenu =  new Menu("Menu Principal", Arrays.asList("Conta", "Cliente", "Operacoes"));
 		int selection = mainMenu.getSelection();
 		if (selection == 3) {
 			executarOperacoes();
+			salvarDados();
 		} else {
 			System.out.println(selection + " foi selecionada");
 		}
@@ -65,12 +73,53 @@ public class Main {
 			double saque = conta.sacar(valor);
 			if (saque > 0) {
 				System.out.println("Saque realizado.");
+				System.out.println("Saldo atualizado: " + conta.getSaldo());
 			} else {
 				System.out.println("Saque nao realizado.");
 			}
 		}
 
-		System.out.println("Saldo atualizado: " + conta.getSaldo());
+		
 	}
 
+	private static void salvarDados() {
+		try (BufferedWriter writer = new BufferedWriter(new FileWriter(CAMINHO_ARQUIVO))) {
+
+			writer.write("NUMERO_CONTA;SALDO;TIPO_CONTA");
+      		writer.newLine();
+			for (Map.Entry<Integer, Conta> entry : contas.entrySet()) {
+				int numero = entry.getKey();
+				Conta conta = entry.getValue();
+				writer.write(numero + ";" + conta.getSaldo() + ";" + (conta.tipoConta != null ? conta.tipoConta : "Corrente"));
+				writer.newLine();
+			}
+			System.out.println("Dados salvos com sucesso!");
+		} catch (IOException e) {
+			System.out.println("Erro ao salvar os dados: " + e.getMessage());
+		}
+}
+
+	private static void carregarDados() {
+		try (BufferedReader reader = new BufferedReader(new FileReader(CAMINHO_ARQUIVO))) {
+			String linha;
+			while ((linha = reader.readLine()) != null) {
+				String[] partes = linha.split(";");
+				if (partes.length == 3) {
+					int numero = Integer.parseInt(partes[0]);
+					double saldo = Double.parseDouble(partes[1]);
+					String tipo = partes[2];
+
+					Conta conta = new Conta();
+					conta.numeroConta = numero;
+					conta.saldo = saldo;
+					conta.tipoConta = tipo;
+
+					contas.put(numero, conta);
+				}
+			}
+			System.out.println("Dados carregados com sucesso! Contas recuperadas: " + contas.size());
+		} catch (IOException e) {
+			System.out.println("Nenhum histórico de contas encontrado. Iniciando banco vazio.");
+		}
+	}
 }
